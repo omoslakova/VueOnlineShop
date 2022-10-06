2ddb06239fed93211e8c0a8ff0d420f9db35f310
omoslakova <o.moslakova@yandex.by>
2022-10-06 12:37:39 +0300

    initialization

---
+++ perfumeshop/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {
+  },
+  setup() {
+
+    return {
+
+    };
+  },
+};
+
+</script>
+
+
+<style scoped>
+
+</style>
